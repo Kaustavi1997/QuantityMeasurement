8a1f54b8bb9a10430f7590e0e8cbb38529c46653
“Kaustavi <kaustavisen8@gmail.com>
2020-07-11 14:12:16 +0530

    Added:Testcase for feetToInch method

--- src/test/java/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -1,4 +1,14 @@
 package quantitymeasurementtest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import quantitymeasurement.QuantityMeasurement;
+
 public class QuantityMeasurementTest {
+    @Test
+    public void givenFeet_ShouldReturnInch() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double result = quantityMeasurement.feetToInch(2.5);
+        Assert.assertEquals(30,result,0.0);
+    }
 }
