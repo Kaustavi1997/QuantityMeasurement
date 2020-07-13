550945596b2057153baafb42cc2d88a9b451e368
“Kaustavi <kaustavisen8@gmail.com>
2020-07-13 18:53:08 +0530

    Added:TC1.2-Testcase for null check

--- src/test/java/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -1,20 +1,33 @@
 package quantitymeasurementtest;
-
+import exception.QuantityMeasurementException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import quantitymeasurement.service.Feet;
+import quantitymeasurement.model.Feet;
 import quantitymeasurement.service.QuantityMeasurement;
 
 public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement;
+    @Before
+    public void initialize(){
+        quantityMeasurement = new QuantityMeasurement();
+    }
     @Test
     public void givenFeet_ShouldReturnInch() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         double result = quantityMeasurement.feetToInch(2.5);
         Assert.assertEquals(30,result,0.0);
     }
     @Test
-    public void whenGivenTwoSameFeetValue_shouldReturnTrue() {
-        Feet feet = new Feet(0);
-        Assert.assertEquals(feet,new Feet(0));
+    public void whenGivenTwoFeetValue_ifEqual_shouldReturnTrue() {
+        Feet feet = new Feet(0.0);
+        Assert.assertEquals(feet,new Feet(0.0));
+    }
+    @Test
+    public void whenNull_ShouldCheck(){
+        try {
+            Feet feet = new Feet(null);
+        }catch(NullPointerException e){
+            System.out.println("null value provided");
+        }
     }
 }
