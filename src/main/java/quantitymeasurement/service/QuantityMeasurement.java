f4d5b75516dcfab7a473497f581682fec0f48aa5
“Kaustavi <kaustavisen8@gmail.com>
2020-07-13 20:17:06 +0530

    Added:main logic for converting yard to feet

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -5,6 +5,7 @@ public class QuantityMeasurement {
     private final double FEET_TO_YARD_CONVERTER = 0.33;
     private final double INCH_TO_YARD_CONVERTER = 0.027;
     private final double YARD_TO_INCH_CONVERTER = 36;
+    private final double YARD_TO_FEET_CONVERTER = 3;
     public double feetToInch(double feet) {
         return feet*FEET_TO_INCH_CONVERTER;
     }
@@ -19,5 +20,8 @@ public class QuantityMeasurement {
     }
     public double yardToInch(double yard){
         return yard*YARD_TO_INCH_CONVERTER;
+    }
+    public double yardToFeet(double yard){
+        return yard*YARD_TO_FEET_CONVERTER;
     }
 }
