40de31bdb9c87c025ab2e64d036687f91fc40ec5
“Kaustavi <kaustavisen8@gmail.com>
2020-07-15 20:51:34 +0530

    Added:main logic for 0 gallon should return 0 gallon

--- src/main/java/quantitymeasurement/model/Volume.java
+++ src/main/java/quantitymeasurement/model/Volume.java
@@ -1,4 +1,19 @@
 package quantitymeasurement.model;
 
 public class Volume {
+    public enum Unit{ GALLON };
+    private final Volume.Unit unit;
+    private final double value;
+    public Volume(double value, Volume.Unit unit) {
+        this.unit = unit ;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Volume volume = (Volume) o;
+        return Double.compare(volume.value, value) == 0 && unit == volume.unit;
+    }
 }
