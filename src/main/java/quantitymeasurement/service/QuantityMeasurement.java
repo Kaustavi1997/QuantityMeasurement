6c11233bdec1296fb2ffe6903799124ccdf57786
“Kaustavi <kaustavisen8@gmail.com>
2020-07-15 20:27:36 +0530

    Added:main logic for addition

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -7,8 +7,10 @@ public class QuantityMeasurement {
     public double converter(Double value, UnitConverterFactor unitConverterFactor) throws QuantityMeasurementException {
         if(value == null){
             throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE,"Null value given");
-
         }
         return value * unitConverterFactor.value;
+    }
+    public double add(Double value1,Double value2){
+        return value1+value2;
     }
 }
